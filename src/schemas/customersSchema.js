b0581aad455821e2d589fa29db57b7b0d7689657
Davi <daviribeirotesta@gmail.com>
2023-02-13 13:52:19 -0300

    refact customersSchema 2

--- src/schemas/customersSchema.js
+++ src/schemas/customersSchema.js
@@ -2,7 +2,7 @@ import joi from "joi";
 
 export const customerSchema = joi.object({
     name: joi.string().required(),
-    phone: joi.number().required().min(10).max(11),
-    cpf: joi.number().required().min(11).max(11),
+    phone: joi.string().required().min(10).max(11),
+    cpf: joi.string().required().min(11).max(11).regex(/^[0-9]+$/),
     birthday: joi.date().required()
 });
